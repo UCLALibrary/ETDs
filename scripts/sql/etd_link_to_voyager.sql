90ec75735d848a242a797064527747ffcd16446d
Andy Kohler <akohler@library.ucla.edu>
2023-08-18 14:49:19 -0800

    Remove old Voyager SQL scripts

--- scripts/sql/etd_link_to_voyager.sql
+++
@@ -1,2 +0,0 @@
--- Updates ETD tables with Voyager data and statuses.
-exec vger_support.etd_link_to_voyager;
--- scripts/sql/etd_status_add.sql
+++
@@ -1,8 +0,0 @@
--- Adds a row to the etd_status table.
--- Parameters from caller:
--- &1: ProQuest id (required)
--- &2: Status (required)
--- &3: Message (optional)
-
-exec vger_support.etd_status_add ('&1', '&2', '&3');
-
